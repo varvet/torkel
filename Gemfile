210b6113e452da96f66269c68aafb806cb866988
Anders Carlsson <andersc@elabs.se>
2013-04-12 09:19:53 +0200

    generated

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'activerecord-jdbcpostgresql-adapter'
+gem 'jruby-openssl'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyrhino'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Torkel::Application.config.secret_token = 'b52d465e8a9f60c18db509e254cb20f07fce4ab35577dc1896dfe77ace0e37233f858d7ff88220f8cafffb8ecab7acb6604cb0e83f89453afa561b9adc104bed'
